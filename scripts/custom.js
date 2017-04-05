e280328b5ec663b93cec881dee1314eb8f3c05dd
stuartsimonsen <stuartupwork666@gmail.com>
2017-04-05 12:09:48 -0600

    Added styling for mobile navigation

--- scripts/custom.js
+++ scripts/custom.js
@@ -3,3 +3,10 @@ $(document).ready(function() {
   $('.content-container').addClass('loaded');
   
 });
+
+$('.mobile-nav-toggle').click( function() {
+  $('body').toggleClass('mobile-nav-open');
+});
+$('.body-overlay').click( function() {
+  $('body').removeClass('mobile-nav-open');
+});
