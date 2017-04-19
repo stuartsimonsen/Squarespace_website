deb88d4e253a403cabef1ff2f5ae500d6646c948
stuartsimonsen <stuartupwork666@gmail.com>
2017-04-19 14:35:30 -0600

    Updated folder script

--- scripts/custom.js
+++ scripts/custom.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   $('.content-container').addClass('loaded');
   $('.folder').addClass('folder-closed');
+  $('.subnav').slideUp();
   
 });
 
@@ -11,9 +12,7 @@ $('.mobile-nav-toggle').click( function() {
 $('.body-overlay').click( function() {
   $('body').removeClass('mobile-nav-open');
 });
-$('.folder-closed .folder-toggle-label').click( function() {
-  $(this).parent().removeClass('folder-closed').addClass('folder-open');
-});
-$('.folder-open .folder-toggle-label').click( function() {
-  $(this).parent().removeClass('folder-open').addClass('folder-closed');
+$('.folder .folder-toggle-label').click( function() {
+  $(this).parent().toggleClass('folder-closed');
+  $(this).next().slideToggle();
 });
