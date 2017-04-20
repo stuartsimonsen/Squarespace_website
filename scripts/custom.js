5ed2dd3b8f61e774af7e2eda67f6789ee1bda94d
stuartsimonsen <stuartupwork666@gmail.com>
2017-04-20 14:04:14 -0600

    Added comments

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,19 +1,23 @@
+
 $(document).ready(function() {
 
-  $('img').addClass('loaded');
-  $('.content-container').addClass('loaded');
-  $('.folder').addClass('folder-closed');
-  $('.subnav').slideUp();
-  $('.loading-overlay').delay(800).fadeOut();
+  $('img').addClass('loaded'); // Make sure images get the loaded class
+  $('.content-container').addClass('loaded'); // Show main content area
+  $('.folder').addClass('folder-closed'); // Close folder navigations
+  $('.subnav').slideUp(); // Close folder navigations
+  $('.loading-overlay').delay(800).fadeOut(); // Hide loading overlay once content is loaded
   
 });
 
+// Show/Hide mobile nav on tap
 $('.mobile-nav-toggle').click( function() {
   $('body').toggleClass('mobile-nav-open');
 });
+// Hide mobile nav when tapping outside the nav area
 $('.body-overlay').click( function() {
   $('body').removeClass('mobile-nav-open');
 });
+// Show/Hide folder subnavs
 $('.folder .folder-toggle-label').click( function() {
   $(this).parent().toggleClass('folder-closed');
   $(this).next().slideToggle();
